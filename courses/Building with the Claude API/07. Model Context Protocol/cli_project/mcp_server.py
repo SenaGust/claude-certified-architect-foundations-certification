e2bc26f8b41cf950e1aa2dacd073a570799ec549
SenaGust <gustavosena98@live.com>
2026-06-11 15:06:08 -0300

    added two tools

--- courses/Building with the Claude API/07. Model Context Protocol/cli_project/mcp_server.py
+++ courses/Building with the Claude API/07. Model Context Protocol/cli_project/mcp_server.py
@@ -1,4 +1,5 @@
 from mcp.server.fastmcp import FastMCP
+from pydantic import Field
 
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
 
@@ -13,12 +14,35 @@ docs = {
 }
 
 # TODO: Write a tool to read a doc
+@mcp.tool(
+        name="read_docs_content", 
+        description="Read the contents of a document and return it as a string."
+)
+def read_docs_content_fn(file_name: str=Field(description="name of the document")):
+    if file_name not in docs:
+        raise ValueError(f"Error: Document with name {file_name} not found")
+
+    return docs[file_name]
+
 # TODO: Write a tool to edit a doc
+@mcp.tool(
+    name="edit_docs_content",
+    description="Edit a document by replacing a string in the documents content with a new string"
+)
+def edit_docs_content_fn(
+    file_name: str = Field(description="name of the document that will be edited"),
+    old_str: str = Field(description="The text to replace. Must match exactly, including whitespace"),
+    new_str: str = Field(description="the new text to insert in place of the old text")
+):
+    if file_name not in docs:
+        raise ValueError(f"Error: Document with name {file_name} not found")
+    
+    docs[file_name] = docs[file_name].replace(old_str, new_str)
+
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
 # TODO: Write a prompt to summarize a doc
-
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
